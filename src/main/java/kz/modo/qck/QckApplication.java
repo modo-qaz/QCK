6cb1ea9f377681108c6375525be3257f8b80e021
chpolzhik <a.olzhas.a@gmail.com>
2022-05-17 18:57:55 +0600

    initial commit

---
+++ src/main/java/kz/modo/qck/QckApplication.java
@@ -0,0 +1,14 @@
+package kz.modo.qck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QckApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(QckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/modo/qck/QckApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.modo.qck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
